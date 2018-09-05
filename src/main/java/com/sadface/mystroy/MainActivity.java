f478bc3cc1c7bbedecbce48ad23b1eb1622a6aa0
americococo <tmdrb515060@gmail.com>
2018-09-05 22:14:14 +0900

    andorid My Story First

---
+++ src/main/java/com/sadface/mystroy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sadface.mystroy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.Text);
+        textView.setText("Hello");
+
+    }
+
+
+
+}
